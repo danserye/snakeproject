68aaf22fc98cd6dac1cd514c46df9be176e3f513
patjlynch360 <118766225+patjlynch360@users.noreply.github.com>
2022-11-22 15:51:13 -0500

    Add files via upload

--- final.c
+++ final.c
@@ -2,19 +2,23 @@
 #include <ncurses.h>
 #include <unistd.h>
 
+#define UP    1
+#define DOWN  2
+#define LEFT  3
+#define RIGHT 4
+
 struct snake{
     int xloc;
     int yloc;
     int head;
 };
 
+void updateSnake(struct snake[], int, int);
+
 int main(){
-    int up = 1;
-    int down = 2;
-    int left = 3;
-    int right = 4;
-    int dir = 4;
+    int dir = RIGHT;
     int x, y, xMax, yMax, ch;
+    int length = 5;
     initscr();
         clear();
         x = y = 1;
@@ -22,23 +26,23 @@ int main(){
         yMax = LINES-1;
 
         //Creates the window
-        WINDOW *win = newwin(LINES-1, COLS-1, 1,1);
+        WINDOW *win = newwin(yMax, xMax, 1,1);
         // grabs key inputs
         keypad(win,true);
         //turns of blocking
         nodelay(win, TRUE);
         //creates the pit
-        box(win,'|','-');
+        box(win,0,0);
 
         //creates the snake
         struct snake snakearr[(xMax*yMax)/2];
-        for(int i=0;i<5;i++){
+        for(int i=0;i<length;i++){
             snakearr[i].xloc=i+1;
             snakearr[i].yloc=1;
             if (i == 4)
                 snakearr[i].head = 1;
         }
-        for(int i=0; i <5; i++){
+        for(int i=0; i <length; i++){
             mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,'o');
         }
 
@@ -46,65 +50,80 @@ int main(){
         //moves the snake by coping the snakes position from the snake struct in front of it unless it is the head of snake then that will move depending on the key that is pressed
         while(1){
             ch = wgetch(win);
-            if(ch == KEY_UP) dir = up;
-            else if(ch == KEY_DOWN) dir = down;
-            else if(ch == KEY_LEFT) dir = left;
-            else if(ch == KEY_RIGHT) dir = right;
-            for(int i=0; i <5; i++){
+            if(ch == KEY_UP && dir != DOWN) dir = UP;
+            else if(ch == KEY_DOWN && dir != UP) dir = DOWN;
+            else if(ch == KEY_LEFT && dir != RIGHT) dir = LEFT;
+            else if(ch == KEY_RIGHT && dir != LEFT) dir = RIGHT;
+            for(int i=0; i <length; i++){
                     mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,' ');
                 }
-            if (dir == up){
-                for(int i = 0;i<5;i++){
-                    if(snakearr[i].head){
-                        snakearr[i].yloc--;
-                    }
-                    else{
-                        snakearr[i].xloc = snakearr[i+1].xloc;
-                        snakearr[i].yloc = snakearr[i+1].yloc;
-                    }
-                }
+            if (dir==UP){
+                updateSnake(snakearr, length, dir);
             }
-            if (dir == down){
-                for(int i = 0;i<5;i++){
-                    if(snakearr[i].head){
-                        snakearr[i].yloc++;
-                    }
-                    else{
-                        snakearr[i].xloc = snakearr[i+1].xloc;
-                        snakearr[i].yloc = snakearr[i+1].yloc;
-                    }
-                }
+            if (dir==DOWN){
+                updateSnake(snakearr, length, dir);
             }
-            if (dir==left){
-                for(int i = 0;i<5;i++){
-                    if(snakearr[i].head){
-                        snakearr[i].xloc--;
-                    }
-                    else{
-                        snakearr[i].xloc = snakearr[i+1].xloc;
-                        snakearr[i].yloc = snakearr[i+1].yloc;
-                    }
-                }
+            if (dir==LEFT){
+                updateSnake(snakearr, length, dir);
             }
-            if (dir==right){
-                for(int i = 0;i<5;i++){
-                    if(snakearr[i].head){
-                        snakearr[i].xloc++;
-                    }
-                    else{
-                        snakearr[i].xloc = snakearr[i+1].xloc;
-                        snakearr[i].yloc = snakearr[i+1].yloc;
-                    }
-                }
+            if (dir==RIGHT){
+                updateSnake(snakearr, length, dir);
             }
             //prints the snake
-            for(int i=0; i <5; i++){
+            for(int i=0; i <length; i++){
                 mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,'o');
             }
-        }
-    wrefresh(win);  
-    usleep(250000);      
+            wrefresh(win);
+            usleep(250000);  
+        }      
     wrefresh(win);
     endwin();
 return 0;
+}
+
+void updateSnake(struct snake arr[], int length, int dir){
+    if (dir == UP){
+        for(int i = 0;i<length;i++){
+            if(arr[i].head){
+                arr[i].yloc--;
+            }
+            else{
+                arr[i].xloc = arr[i+1].xloc;
+                arr[i].yloc = arr[i+1].yloc;
+            }
+        }
+    }
+    if (dir == DOWN){
+        for(int i = 0;i<length;i++){
+            if(arr[i].head){
+                arr[i].yloc++;
+            }
+            else{
+                arr[i].xloc = arr[i+1].xloc;
+                arr[i].yloc = arr[i+1].yloc;
+            }
+        }
+    }
+    if (dir==LEFT){
+        for(int i = 0;i<length;i++){
+            if(arr[i].head){
+                arr[i].xloc--;
+            }
+            else{
+                arr[i].xloc = arr[i+1].xloc;
+                arr[i].yloc = arr[i+1].yloc;
+            }
+        }
+    }
+    if (dir==RIGHT){
+        for(int i = 0;i<length;i++){
+            if(arr[i].head){
+                arr[i].xloc++;
+            }
+            else{
+                arr[i].xloc = arr[i+1].xloc;
+                arr[i].yloc = arr[i+1].yloc;
+            }
+        }
+    }
 }
