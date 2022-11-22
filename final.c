6f677ec5e1e89e63b925898f0d9fad2889c79f44
patjlynch360 <118766225+patjlynch360@users.noreply.github.com>
2022-11-21 20:40:59 -0500

    Add files via upload

--- final.c
+++ final.c
@@ -9,16 +9,25 @@ struct snake{
 };
 
 int main(){
-    int x, y, xMax, yMax;
+    int up = 1;
+    int down = 2;
+    int left = 3;
+    int right = 4;
+    int dir = 4;
+    int x, y, xMax, yMax, ch;
     initscr();
-        echo();
+        clear();
         x = y = 1;
         xMax = COLS-1;
         yMax = LINES-1;
 
-        //Creates the Snake Pit
+        //Creates the window
         WINDOW *win = newwin(LINES-1, COLS-1, 1,1);
+        // grabs key inputs
         keypad(win,true);
+        //turns of blocking
+        nodelay(win, TRUE);
+        //creates the pit
         box(win,'|','-');
 
         //creates the snake
@@ -36,78 +45,66 @@ int main(){
         wrefresh(win);
         //moves the snake by coping the snakes position from the snake struct in front of it unless it is the head of snake then that will move depending on the key that is pressed
         while(1){
-            int ch = wgetch(win);
-            switch(ch){
-                case KEY_UP:
-                    for(int i=0; i <5; i++){
-                        mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,' ');
+            ch = wgetch(win);
+            if(ch == KEY_UP) dir = up;
+            else if(ch == KEY_DOWN) dir = down;
+            else if(ch == KEY_LEFT) dir = left;
+            else if(ch == KEY_RIGHT) dir = right;
+            for(int i=0; i <5; i++){
+                    mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,' ');
+                }
+            if (dir == up){
+                for(int i = 0;i<5;i++){
+                    if(snakearr[i].head){
+                        snakearr[i].yloc--;
                     }
-                    for(int i = 0;i<5;i++){
-                        if(snakearr[i].head){
-                            snakearr[i].yloc--;
-                        }
-                        else{
-                            snakearr[i].xloc = snakearr[i+1].xloc;
-                            snakearr[i].yloc = snakearr[i+1].yloc;
-                        }
+                    else{
+                        snakearr[i].xloc = snakearr[i+1].xloc;
+                        snakearr[i].yloc = snakearr[i+1].yloc;
                     }
-                    break;
-                case KEY_DOWN:
-                    for(int i=0; i <5; i++){
-                        mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,' ');
-                    }
-                    for(int i = 0;i<5;i++){
-                        if(snakearr[i].head){
-                            snakearr[i].yloc++;
-                        }
-                        else{
-                            snakearr[i].xloc = snakearr[i+1].xloc;
-                            snakearr[i].yloc = snakearr[i+1].yloc;
-                        }
-                    }
-                    
-                    break;
-                case KEY_LEFT:
-                    for(int i=0; i <5; i++){
-                        mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,' ');
-                    }
-                    for(int i = 0;i<5;i++){
-                        if(snakearr[i].head){
-                            snakearr[i].xloc--;
-                        }
-                        else{
-                            snakearr[i].xloc = snakearr[i+1].xloc;
-                            snakearr[i].yloc = snakearr[i+1].yloc;
-                        }
-                    }
-                    
-                    break;
-                case KEY_RIGHT:
-                    for(int i=0; i <5; i++){
-                        mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,' ');
-                    }
-                    for(int i = 0;i<5;i++){
-                        if(snakearr[i].head){
-                            snakearr[i].xloc++;
-                        }
-                        else{
-                            snakearr[i].xloc = snakearr[i+1].xloc;
-                            snakearr[i].yloc = snakearr[i+1].yloc;
-                        }
-                    }
-                    
-                    break;
-                default:
-                    break;
+                }
             }
+            if (dir == down){
+                for(int i = 0;i<5;i++){
+                    if(snakearr[i].head){
+                        snakearr[i].yloc++;
+                    }
+                    else{
+                        snakearr[i].xloc = snakearr[i+1].xloc;
+                        snakearr[i].yloc = snakearr[i+1].yloc;
+                    }
+                }
+            }
+            if (dir==left){
+                for(int i = 0;i<5;i++){
+                    if(snakearr[i].head){
+                        snakearr[i].xloc--;
+                    }
+                    else{
+                        snakearr[i].xloc = snakearr[i+1].xloc;
+                        snakearr[i].yloc = snakearr[i+1].yloc;
+                    }
+                }
+            }
+            if (dir==right){
+                for(int i = 0;i<5;i++){
+                    if(snakearr[i].head){
+                        snakearr[i].xloc++;
+                    }
+                    else{
+                        snakearr[i].xloc = snakearr[i+1].xloc;
+                        snakearr[i].yloc = snakearr[i+1].yloc;
+                    }
+                }
+            }
+            //prints the snake
             for(int i=0; i <5; i++){
                 mvwaddch(win,snakearr[i].yloc,snakearr[i].xloc,'o');
             }
-            wrefresh(win);
         }
-        getchar();
+    wrefresh(win);  
+    usleep(250000);      
+    wrefresh(win);
     endwin();
 return 0;
 }
-
-
